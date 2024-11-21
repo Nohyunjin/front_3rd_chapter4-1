f6472e3db4168ac4aae80a9134617113858cdfe5
hjnoh <hyunjin.noh@saltlux.com>
2024-11-22 00:36:41 +0900

    [fix] 오타 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
             {[
               'AGENT',
-              'LM',
+              'LLM',
               'MRC',
               'STT',
               'TTS',
